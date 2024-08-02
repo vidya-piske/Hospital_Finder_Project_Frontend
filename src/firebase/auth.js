fab433f91b135588cb1293faea17e55591e76022
vidya-piske <vidyapiske123@gmail.com>
2024-08-02 15:35:09 +0530

    Remove console.log from auth functions

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -22,9 +22,7 @@ export const googleLogin = () => {
 export const signOut = async () => {
   try {
     await firebaseSignOut(auth);
-    console.log('User signed out successfully');
   } catch (error) {
-    console.error('Error signing out:', error.message);
     throw error;
   }
 };
